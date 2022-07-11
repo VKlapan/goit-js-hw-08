84e261eac2e015febddf18a59e1b49e2c9dfedab
VKlapan <vladimir.klapan@gmail.com>
2022-07-11 19:49:36 +0300

    refactoring task#01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//const basicLightbox = require('simplelightbox');
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryMarkup = galleryItems => {
+  const itemLiMarkup = galleryItems
+    .map(item => {
+      return `
+      <div>
+        <a class="gallery__item" href="${item.original}">
+            <img  class="gallery__image"
+                src="${item.preview}"
+                alt="${item.description}">
+        </a>
+      </div>
+        `;
+    })
+    .join('');
+
+  return itemLiMarkup;
+};
+
+galleryEl.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox');
