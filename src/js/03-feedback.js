c9ed6941749a0716fa08824fddcb1b5804df90a5
VKlapan <vladimir.klapan@gmail.com>
2022-07-15 17:58:31 +0300

    fix currentFeedbackFormState behavior and init input values

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,19 +5,24 @@ const emailInputEl = formEl.querySelector('input[name="email"]');
 const msgInputEl = formEl.querySelector('textarea[name="message"]');
 const btnSubmitEl = formEl.querySelector('button[type="submit"]');
 
-const currentFeedbackFormState = localStorage.getItem('feedback-form-state')
+let currentFeedbackFormState = localStorage.getItem('feedback-form-state')
   ? JSON.parse(localStorage.getItem('feedback-form-state'))
   : {};
 
-if (localStorage.getItem('feedback-form-state')) {
+if (currentFeedbackFormState.email) {
   emailInputEl.value = currentFeedbackFormState.email;
+}
+
+if (currentFeedbackFormState.message) {
   msgInputEl.value = currentFeedbackFormState.message;
 }
+
 const onSubmit = event => {
   event.preventDefault();
   console.log(currentFeedbackFormState);
   localStorage.removeItem('feedback-form-state');
   formEl.reset();
+  currentFeedbackFormState = {};
 };
 
 const fillInForm = event => {
