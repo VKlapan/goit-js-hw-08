611b25e0842e9d48e87647bc851315b7abae1384
VKlapan <vladimir.klapan@gmail.com>
2022-07-15 17:23:27 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const formEl = document.querySelector('.feedback-form');
+const emailInputEl = formEl.querySelector('input[name="email"]');
+const msgInputEl = formEl.querySelector('textarea[name="message"]');
+const btnSubmitEl = formEl.querySelector('button[type="submit"]');
 
+const currentFeedbackFormState = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state'))
+  : {};
+
+if (localStorage.getItem('feedback-form-state')) {
+  emailInputEl.value = currentFeedbackFormState.email;
+  msgInputEl.value = currentFeedbackFormState.message;
+}
+const onSubmit = event => {
+  event.preventDefault();
+  console.log(currentFeedbackFormState);
+  localStorage.removeItem('feedback-form-state');
+  formEl.reset();
+};
+
+const fillInForm = event => {
+  currentFeedbackFormState[event.target.name] = event.target.value;
+  console.log(currentFeedbackFormState);
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(currentFeedbackFormState)
+  );
+
+  console.log(localStorage.getItem('feedback-form-state'));
+};
+
+btnSubmitEl.addEventListener('click', onSubmit);
+formEl.addEventListener('input', fillInForm);
